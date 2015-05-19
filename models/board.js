233ca178cd292582427d4ddfe05a8b76095befe7
florida <florida.elago@gmail.com>
2015-05-19 10:17:42 -0700

    add board schema

--- models/board.js
+++ models/board.js
@@ -5,3 +5,11 @@ function required(val) { return val && val.length; }
 
 var Schema = mongoose.Schema;
 var ObjectID = Schema.ObjectID;
+
+var BoardSchema = new Schema({
+  name: {
+    type: String,
+    validate: [required, 'Name is required']
+  },
+  clips: [] //TODO: Add clip type
+})
